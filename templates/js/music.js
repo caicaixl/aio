f51af1099d467f3621172e3705df5dc55c80ae7b
61676 <ab616767071@gmail.com>
2019-06-29 18:52:38 +0800

    update

--- templates/js/music.js
+++ templates/js/music.js
@@ -366,12 +366,20 @@ function hengshuping() {
 
 window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
 
-// $(document).ready(function () {
+$(document).ready(function () {
 //         // $('.fa-play-circle')[0].click();
 //         // musicfc = document.getElementsByTagName("audio")[0];
 //         // musicfc.play();
 //         autoPlay()
-//     });
+    if(typeof WeixinJSBridge === 'object'){
+            // musicfc = document.getElementsByTagName("audio")[0];
+     WeixinJSBridge.invoke('getNetworkType', {}, function(e) {
+        musicfc.play();
+     });
+}else{
+    musicfc.play()
+}
+    });
 // $(function){$('i[class="fa fa fa-play-circle play"]').click()};
 
 // i[class="fa fa-play-circle play"]
